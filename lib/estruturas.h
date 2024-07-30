a095d341822db25212e0d8a4e7aa3547ce69f48b
JuliaMR <juliamrz2@outlook.com>
2024-07-30 20:25:43 -0300

    Adiciona função de tratamento de pacore

--- lib/estruturas.h
+++ lib/estruturas.h
@@ -1,9 +1,11 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include<string.h>
 
 #define MAX_STRING 255
 #define NUM_CONTATOS 10
 #define NUM_MSG 5
+#define DELIMITER "|"
 
 // Talvez necessário
 typedef struct{
@@ -20,3 +22,47 @@ typedef struct {
 
 contato lista_contatos[NUM_CONTATOS];
 // 1|002|matheus|ip|porta|oi
+
+
+
+// Função para tratar pacote e redirecionar para o tipo de operação
+void tratar_pacote(char* pacote){
+
+	int ito = strtok(pacote, DELIMITER);
+
+	int tam_pacote = strtok(NULL, DELIMITER);
+
+	switch (ito)
+	{
+	case 0:
+		// ERRO
+		break;
+	case 1:
+		// SUCESSO
+		break;
+	case 2:
+		// DEVOLVE LISTA
+		break;
+	case 3:
+		// SOLICITAÇÃO DE REGISTRO
+		break;
+	case 4:
+		// SOLICITAÇÃO DE LISTA
+		break;
+	case 5:
+		// MANDAR MENSAGEM
+		break;
+	case 6:
+		// MANDAR MENSAGEM BROADCAST
+		break;
+	case 7:
+		// SOLICITAÇÃO PARA DEDSCONECTAR
+		break;
+	default:
+		break;
+	}
+}
+
+// Função para tratar mensagem 
+
+// Função para criar contato
