683757eb65b035df21fc36f6a1f35396ad00d06d
JuliaMR <juliamrz2@outlook.com>
2024-08-11 22:30:40 -0300

    add

---
+++ lib/defines.h
@@ -0,0 +1,10 @@
+#define MAX_STRING 255
+#define NUM_CONTATOS 10
+#define NUM_MSG 5
+#define MAX_NAME 10
+#define DELIMITER "|"
+#define ERRO 1
+#define OK 0
+
+#define PORTA_SERVIDOR 8888
+#define MAXPENDING 5
