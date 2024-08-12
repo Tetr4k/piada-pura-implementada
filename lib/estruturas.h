d0a60b0b38bf9663e7d73445d6ef1425dacb3c67
JuliaMR <juliamrz2@outlook.com>
2024-08-11 22:23:18 -0300

    add criar_mensagem_lista_de_contato and aux functions

--- lib/estruturas.h
+++ lib/estruturas.h
@@ -30,7 +30,7 @@ void tratar_pacote(char* pacote){
 
 	int ito = atoi(strtok(pacote, DELIMITER));
 	int tam_pacote = atoi(strtok(NULL, DELIMITER));
-
+	
 	switch (ito)
 	{
 	case 0:
@@ -77,4 +77,36 @@ void m_print_contato(contato tmp){
     printf("porta: %d\n", tmp.porta);
 }
 
-// Função para criar contato
+bool contato_esta_vazio(contato tmp){
+	bool result = (tmp.nome[0] == '\0') && (tmp.ip[0] == '\0') && (tmp.porta == 0);
+	return result;
+}
+// what: concatena contato a mensagem
+void m_concat_str(char** dest, contato contato){
+    char tmp[40];
+    sprintf(tmp, "|%s|%s|%d", contato.nome, contato.ip, contato.porta);
+ 	int tamanho_total = strlen(*dest) + strlen(tmp); // +1 for caractere +1 for '\0'
+    *dest = realloc(*dest, tamanho_total);
+    strcat(*dest,tmp);
+}
+// return: string de mensagem (type: char*)
+char* criar_mensagem_lista_de_contato(){
+	char* PORTA;
+	char* msg = malloc(1);
+	char* pacote = malloc(5);
+	int pacote_tam;
+	for(int i = 0; i < NUM_CONTATOS; i++){ // itera em vetor de contato até chegar em campo vazio ou fim de vetor
+		if(contato_esta_vazio(lista_contatos[i])){
+			printf("Saindo\n");
+			break;
+		}
+		m_concat_str(&msg, lista_contatos[i]); // transforma contato em string
+	}
+	
+	// montagem do pacote
+	sprintf(pacote, "2|%03d", strlen(msg)-1);
+	pacote_tam = strlen(pacote) + strlen(msg);
+	pacote = realloc(pacote, pacote_tam);
+	strcat(pacote, msg);
+	return pacote;
+}
