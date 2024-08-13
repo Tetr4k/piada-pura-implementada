917369d753b6803277ac1b7dcb1235c3295dea39
Citeli-py <matheus.citeli2001@gmail.com>
2024-08-13 13:57:03 -0300

    Junção dos comentários para auxilio e substituição de server_ip para ds_ip

--- lib/socketEnvia.h
+++ lib/socketEnvia.h
@@ -34,42 +34,16 @@ Servidor:
     enviar_mensagem(mensagem, sock);      4
 */
 
-/*
-Cliente:
-"4|000|nome|ip|porta", contato    
-int sock = socket_envia(contato.ip, contato.porta, "4|000|nome|ip|porta");
--> socket_envia
-    enviar_mensagem(mensagem, sock);    1
-    receber_mensagem(mensagem, sock);   5
-    5|100|nome|ip|porta|....
-    tratar_mensagem(mensagem);          6
-
-
-Servidor:
--> socket_escuta
-    receber_mensagem(mensagem, sock);     2
-    tratar_mensagem(mensagem);            3
-    5|100|nome|ip|porta|....
-    enviar_mensagem(mensagem, sock);      4
-*/
-
-int socket_envia(char* dst_ip, int dst_porta, char* mensagem){
+int socket_envia(char* server_ip, int porta_servidor, char* mensagem){
     int sock = criar_socket(0);
     
-    if(conectar(sock, dst_ip, dst_porta)<0) 
+    if(conectar(sock, server_ip, porta_servidor)<0) 
         return -1;
 
-    // Mensagem já foi construida fora do socket
+    // Aqui vem a criação das mensagens
     enviar_mensagem(mensagem, sock);
-
-    /*
-    // Com tratar_mensagem
     receber_mensagem(mensagem, sock);
-    tratar_mensagem(mensagem);
-    */
-
-    //Sem tratamento da resposta
-    receber_mensagem(mensagem, sock);
+    //Tratamento da resposta
 
     close(sock);
     return 1;
